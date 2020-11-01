338204d4d9f47cad28609c63b53459a2040f413b
Timxxx <tbenker@gmx.ch>
2020-11-01 15:52:08 +0100

    offiers are now fully implemented

--- models/Officer.js
+++ models/Officer.js
@@ -1,5 +1,6 @@
 // IMPORTS
 const mongoose = require("mongoose")
+var uniqueValidator = require('mongoose-unique-validator');
 
 // Schema for officers
 const Officer = mongoose.Schema({
@@ -13,9 +14,12 @@ const Officer = mongoose.Schema({
     },
     badgeNumber: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     personRecords: [{type: String, ref: "PersonRecord"}]
 })
+
+Officer.plugin(uniqueValidator);
 
 module.exports = mongoose.model("Officer", Officer)
--- routes/officers.js
+++ routes/officers.js
@@ -4,12 +4,31 @@ const Record = require("../models/PersonRecord")
 const Officer = require("../models/Officer")
 const mongoose = require("mongoose")
 
-// @desc    Get All Records
-// @route   GET /records
+// @desc    Get All Officers
+// @route   GET /officers
 router.get("/", async (req, res) => {
     try {
         const officers = await Officer.find().populate("personRecords")
         res.status(200).json(officers)
+    } catch (error) {
+        res.json({ message: error })
+    }
+})
+
+// @desc    Create Single Officers
+// @route   POST /officers
+router.post("/", async (req, res) => {
+    const officer = new Officer({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        badgeNumber: req.body.badgeNumber
+    })
+
+    console.log(req.body.firstName, req.body.lastName)
+
+    try {
+        const savedOfficer = await officer.save()
+        res.json(savedOfficer)
     } catch (error) {
         res.json({ message: error })
     }
